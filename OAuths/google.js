e6ecae69e86716b3cdfd59a6b64b5228687f928b
Stanyke <Chibuike.stanley50@gmail.com>
2021-02-09 22:06:43 +0100

    Added env

--- OAuths/google.js
+++ OAuths/google.js
@@ -5,7 +5,7 @@ const User = require("../model/User")
 const GooglePassport = passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/auth/google/callback"
+  callbackURL: "https://social-media-auth.herokuapp.com/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
       const userData= {
           "id": profile._json.sub,
